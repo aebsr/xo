69ccdaadee2cc037a82f449f0379adb97c2f5930
Antoine Butler <antoinebsr@gmail.com>
2020-07-27 12:08:34 -0400

    fix close aside position

--- src/components/Aside.js
+++ src/components/Aside.js
@@ -4,7 +4,12 @@ const Nav  = (props) => {
   const openState = props.asideVisible ? 'aside_is-open' : ''
   return (
     <aside className={`aside ${openState}`}>
-      <div className="aside__header">Info</div>
+      <div className="aside__header">
+        <h2>Info</h2>
+        <button className="close-aside" onClick={props.close}>
+          <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
+        </button>
+      </div>
       <div className="aside__content">
         <h3>Goal</h3>
         <p>In order to win the game, a player must place three of their marks in a horizontal, vertical, or diagonal row.</p>
@@ -25,9 +30,6 @@ const Nav  = (props) => {
           </li>
         </ul>
       </div>
-      <button className="close-aside" onClick={props.close}>
-      <svg aria-hidden="true" focusable="false" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 352 512"><path d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"></path></svg>
-      </button>
     </aside>
   )
 }
